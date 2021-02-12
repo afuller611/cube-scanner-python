d4db4902e4b47d14ba4a28a2b8149c0db8507073
Spencer Wood <spencerwood9@gmail.com>
2021-02-11 17:42:47 -0700

    scanned sides

--- scanAllSides.py
+++ scanAllSides.py
@@ -1,9 +1,24 @@
 
+import time
 
+scanned_sides = 0
 # While scanned sides is less than 7
+while(scanned_sides < 7):
+    # Scan side
+    # Take screenshot of scanned image
+    if (scanned_sides == 1 or scanned_sides == 2 or scanned_sides == 3):
+        print("Rotate Cube to the Left")
+        time.sleep(5)
+    if (scanned_sides == 4):
+        print("Rotate Cube Up")
+        time.sleep(5)
+    if (scanned_sides == 5):
+        print("Rotate Cube Up Twice")
+        time.sleep(5)
 
-# Scan side
-# Take screenshot of scanned image
+    scanned_sides += 1
+
+
 
 # if scanned sides length < 4
 # Next side text is "Rotate to left"
